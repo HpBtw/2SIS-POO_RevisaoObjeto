79ff4a5beb48912a49e3c9a524bbf73d8d341341
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-02 22:42:29 -0300

    Finalizar depois, revisao 02/04

---
+++ src/domain/Bola.java
@@ -0,0 +1,26 @@
+package domain;
+
+public class Bola {
+    private String cor;
+    private double raio;
+
+    public Bola(String cor, double ratio) {
+        this.cor = cor;
+        this.raio = raio;
+    }
+
+    public Bola maiorBola(Bola bola2, Bola bola3) {
+        Bola aux = this;
+        if (bola2.raio > bola3.raio && bola2.raio > this.raio) {
+            aux = bola2;
+        }
+        else if (bola3.raio > bola2.raio && bola3.raio > this.raio) {
+            aux = bola3;
+        }
+        return aux;
+    }
+
+    public String retornarDados() {
+        return cor + " " + raio;
+    }
+}
